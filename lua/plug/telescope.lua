b1741146deb49d9c4f5c8d7822d4b352c8eed927
nileshjawarkar <jawarkar.nilesh@gmail.com>
2024-05-25 22:16:13 +0530

    Modified telescope keymaps.

--- lua/plug/telescope.lua
+++ lua/plug/telescope.lua
@@ -12,6 +12,9 @@ return {
 				mappings = {
 					i = {
 						["jk"] = actions.close,
+                        ["C-h"] = actions.move_selection_previous,
+                        ["C-l"] = actions.move_selection_next,
+                        ["C-q"] = actions.send_selected_to_qflist + actions.open_qflist
 					},
 				},
 				set_env = { ["COLORTERM"] = "truecolor" },
@@ -35,9 +38,11 @@ return {
 		})
 
 		local builtin = require("telescope.builtin")
-		vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-		vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-		vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
-		vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+		vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files"})
+		vim.keymap.set("n", "<leader>fr", builtin.oldfiles, { desc = "Find recent open files"})
+		vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Find text" })
+		vim.keymap.set("n", "<leader>fc", builtin.grep_string, { desc = "Find text under cursor" })
+		vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "List open files" })
+		vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Show help tags" })
 	end,
 }
