3ab012bbe20f776ec90f811cba0e9bb33badc32a
nileshjawarkar <jawarkar.nilesh@gmail.com>
2024-06-08 10:58:47 +0530

    Modified whichkey usage

--- lua/plug/whichkey.lua
+++ lua/plug/whichkey.lua
@@ -3,24 +3,26 @@ return {
   event = "VeryLazy",
   config =  function()
     vim.o.timeout = true
-    vim.o.timeoutlen = 500
+    vim.o.timeoutlen = 550
     local wk = require("which-key")
     wk.setup({
-         window = {
+        window = {
             border = "single", -- none, single, double, shadow
             position = "bottom", -- bottom, top
-            margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]. When between 0 and 1, will be treated as a percentage of the screen size.
-            padding = { 1, 2, 1, 2 }, -- extra window padding [top, right, bottom, left]
-            winblend = 0, -- value between 0-100 0 for fully opaque and 100 for fully transparent
+            margin = { 1, 0, 0, 0 }, -- extra window margin [top, right, bottom, left]. When between 0 and 1, will be treated as a percentage of the screen size.
+            padding = { 1, 1, 1, 1 }, -- extra window padding [top, right, bottom, left]
+            winblend = 5, -- value between 0-100 0 for fully opaque and 100 for fully transparent
             zindex = 1000, -- positive value to position WhichKey above other floating windows.
-         },
-         layout = {
+        },
+        layout = {
             height = { min = 4, max = 25 }, -- min and max height of the columns
             width = { min = 20, max = 50 }, -- min and max width of the columns
-            spacing = 3, -- spacing between columns
+            spacing = 2, -- spacing between columns
             align = "left", -- align columns left, center or right
-         },
-        --ignore_missing = true,
+        },
+        ignore_missing = true,
+        show_help = false, -- show a help message in the command line for using WhichKey
+        show_keys = false, -- show the currently pressed key and its label as a message in the command line
     })
     wk.register({
             f = {
@@ -32,9 +34,7 @@ return {
             g = {
                 name = "Git"
             },
-            w = {
-                name = "Window Navigation"
-            },
+            ["w"] = "which_key_ignore",
             ["e"] = "which_key_ignore",
             ["q"] = "which_key_ignore",
         }, {
