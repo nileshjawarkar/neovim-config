a16b238cbcd6ab640d13d19e3003a7d56dfae10e
nileshjawarkar <jawarkar.nilesh@gmail.com>
2024-06-15 16:20:57 +0530

    update bootstrap snippets

--- lua/snippets/bootstrap.lua
+++ lua/snippets/bootstrap.lua
@@ -16,10 +16,26 @@ ls.add_snippets("html", {
          <title>{title}</title>
      </head>
      <body>
+         {content}
          <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
          <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
      </body>
      </html>
-     ]], { title = i(1, "Page Title"), }
+     ]], { title = i(1, "Page Title"), content = i(2) }
     )),
+    s("bs_container", fmt([[
+<div class="container">
+    {}
+</div>
+    ]], {i(1),})),
+    s("bs_row", fmt([[
+<div class="row">
+    {}
+</div>
+    ]], {i(1),})),
+    s("bs_col", fmt([[
+<div class="col">
+    {}
+</div>
+    ]], {i(1),}))
 })
