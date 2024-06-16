e330d27f59ebf6c248abc91117e8288dab2fbb82
nileshjawarkar <jawarkar.nilesh@gmail.com>
2024-06-16 17:05:36 +0530

    Added bootstrap snippets

--- lua/snippets/bootstrap.lua
+++ lua/snippets/bootstrap.lua
@@ -1,12 +1,13 @@
 local ls = require("luasnip")
 local s = ls.snippet
--- local t = ls.text_node
+local t = ls.text_node
+local c = ls.choice_node
 local fmt = require('luasnip.extras.fmt').fmt
 local i = ls.insert_node
 local r = require("luasnip.extras").rep
 
 ls.add_snippets("html", {
-    s("bootstrap_starter", fmt([[
+    s("bs_starter", fmt([[
      <!DOCTYPE html>
      <html lang="en">
      <head>
@@ -23,6 +24,47 @@ ls.add_snippets("html", {
          <script src="js/script.js"></script>
      </body>
      </html>
-     ]], {i(1)}
+     ]], { i(1) }
     )),
+    s("bs_navbar", fmt([[
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        {}
+      </ul>
+    </div>
+  </div>
+</nav>
+    ]], { i(1), })),
+    s("bs_nav_item", fmt([[
+<li class="nav-item">
+  <a class="nav-link" href="#">Link</a>
+</li>
+    ]], {})),
+    s("bs_nav_item_disabled", fmt([[
+<li class="nav-item">
+  <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+</li>
+    ]], {})),
+    s("bs_nav_item_dropdown", fmt([[
+<li class="nav-item dropdown">
+  <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+    Dropdown
+  </a>
+  <ul class="dropdown-menu">
+    <li><a class="dropdown-item" href="#">Action</a></li>
+    <li><a class="dropdown-item" href="#">Another action</a></li>
+    <li><hr class="dropdown-divider"></li>
+    <li><a class="dropdown-item" href="#">Something else here</a></li>
+  </ul>
+</li>
+    ]], {})),
 })
