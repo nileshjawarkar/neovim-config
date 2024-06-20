abb3de20c226a09da5f95be45dfdbb4de3b5c89f
nileshjawarkar <jawarkar.nilesh@gmail.com>
2024-06-20 20:34:25 +0530

    Added more treesitter parsers

--- lua/plug/treesitter.lua
+++ lua/plug/treesitter.lua
@@ -1,24 +1,24 @@
 return {
     'nvim-treesitter/nvim-treesitter',
     config = function()
-        require 'nvim-treesitter.configs'.setup {
+        require 'nvim-treesitter.configs'.setup({
             ensure_installed = {
                 "c", "lua", "cpp", "java",
                 "css", "html", "javascript",
-                "json", "make", "yaml",
+                "json", "make", "yaml", "vim", "vimdoc",
             },
             sync_install = false,
+            auto_install = false,
+            ignore_install = {},
             indent = {
                 enable = true
             },
             highlight = {
                 enable = true,
-                -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-                -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-                -- Using this option may slow down your editor, and you may see some duplicate highlights.
-                -- Instead of true it can also be a list of languages
                 additional_vim_regex_highlighting = false,
             },
-        }
+            modules = {
+            },
+        })
     end
 }
