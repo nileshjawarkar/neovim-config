a090b8d2aaa3492da5c4509901c69734ff9a57b2
nileshjawarkar <jawarkar.nilesh@gmail.com>
2024-06-30 20:36:19 +0530

    Add utility module - for reading, writting files

---
+++ lua/core/file_rwa.lua
@@ -0,0 +1,84 @@
+local function open_file(filename, mode)
+    local file = io.open(filename, mode)
+    if file == nil then
+        return nil
+    end
+    return {
+        close = function()
+            file:close()
+        end,
+        read_all = function()
+            return file:read("*a")
+        end,
+        read_line = function()
+            return file:read()
+        end,
+        write = function(data)
+            file:write(data)
+        end,
+        flush = function()
+            file:flush()
+        end,
+        seek_start = function()
+            file:seek("set", 0)
+        end,
+        seek_end = function()
+            file:seek("end", 0)
+        end,
+        seek_from_cur = function(offset)
+            file:seek("cur", offset)
+        end,
+    }
+end
+
+return {
+    read_from = function(filename, callback)
+        local file = open_file(filename, "r")
+        if file ~= nil and callback ~= nil then
+            local rfile = {
+                seek_start = file.seek_start,
+                seek_end = file.seek_end,
+                seek_from_cur = file.seek_from_cur,
+                read_all = file.read_all,
+                read_line = file.read_line,
+            }
+            callback(rfile)
+            file.close()
+            return true
+        end
+        return false
+    end,
+    write_to = function(filename, callback)
+        local file = open_file(filename, "w+")
+        if file ~= nil and callback ~= nil then
+            local wfile = {
+                seek_start = file.seek_start,
+                seek_end = file.seek_end,
+                seek_from_cur = file.seek_from_cur,
+                write = file.write,
+                flush = file.flush,
+            }
+            callback(wfile)
+            file.close()
+            return true
+        end
+        return false
+    end,
+    append_to = function(filename, callback)
+        local file = open_file(filename, "a+")
+        if file ~= nil and callback ~= nil then
+            local afile = {
+                close = file.close,
+                seek_start = file.seek_start,
+                seek_end = file.seek_end,
+                seek_from_cur = file.seek_from_cur,
+                write = file.write,
+                flush = file.flush,
+            }
+            callback(afile)
+            return true
+        end
+        return false
+    end,
+}
+
