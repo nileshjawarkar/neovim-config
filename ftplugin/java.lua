5adc59c75474589d9fe735cf03b38feaf5b41f57
nileshjawarkar <jawarkar.nilesh@gmail.com>
2024-07-05 20:46:15 +0530

    Modified jdtls config to check for JAVA_HOME

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -1,7 +1,13 @@
 local jdtls = require('config.jdtls')
-local version = jdtls.get_java_version().major
-if  version >= 17 then
-    jdtls.setup()
+local version, err = jdtls.get_java_version()
+local jtls_err = "Error - Lsp server (jdtls) start failed."
+if err ~= nil then
+    print(jtls_err .. " " .. err)
+elseif version == nil then
+    print(jtls_err .. " Failed to retrieve java version.")
+elseif  version.major < 17 then
+    print("Error : Current java version \"" .. version.major .. "\", minimum required \"17\".")
 else
-    print("Error : Current java version \"" .. version .. "\", minimum required \"17\".")
+    print("Starting language server ...")
+    jdtls.setup()
 end
--- lua/config/jdtls.lua
+++ lua/config/jdtls.lua
@@ -185,6 +185,20 @@ local function prepare_config()
     }
 end
 
+
+local function get_java_path()
+    local java_home = os.getenv("JAVA_HOME")
+    if java_home ~= nil then
+        local sys = require("core.util.sys")
+        if sys.is_dir(java_home) then
+            return java_home .. "/bin/java", nil
+        else
+            return nil, "JAVA_HOME path \"" .. java_home .. "\" does not exist."
+        end
+    end
+    return "java", nil
+end
+
 return {
     setup = function()
         jdtls.start_or_attach(prepare_config())
@@ -195,10 +209,15 @@ return {
         local project_name = sys.get_cur_dir()
         sys.rm_rf(data_path .. '/workspace/jdtls/' .. project_name)
     end,
+    get_java_path = get_java_path,
     get_java_version = function()
-        local version = { major = 0, minor = 0, patch = 0 }
         local sys = require("core.util.sys")
-        local r = sys.exec_r("java -version 2>&1")
+        local version = { major = 0, minor = 0, patch = 0 }
+        local java_path, err = get_java_path()
+        if err ~= nil then
+            return version, err
+        end
+        local r = sys.exec_r(java_path .. " -version 2>&1")
         if r ~= nil then
             for mv, sv, pv in string.gmatch(r, "[version]+%s+.(%d+)%.(%d+)%.(%d+).%s+") do
                 local major, minor, patch = tonumber(mv), tonumber(sv), tonumber(pv)
@@ -209,7 +228,9 @@ return {
                     break
                 end
             end
+        else
+            err = "Failed to read version using cmd \"" +java_path + " -version\""
         end
-        return version
+        return version, err
     end,
 }
