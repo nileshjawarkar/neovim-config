05d0a8f717cc19e03aea2866d888df90070aab27
nileshjawarkar <jawarkar.nilesh@gmail.com>
2024-07-13 10:40:05 +0530

    whichkey updates - adapted new config rules

--- lua/plug/telescope.lua
+++ lua/plug/telescope.lua
@@ -47,6 +47,7 @@ return {
         vim.keymap.set("n", "<leader>,", builtin.buffers, { desc = "List open files" })
         vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Show help tags" })
         vim.keymap.set("n", "<leader>f.", builtin.current_buffer_fuzzy_find, { desc = "Fuzzy find in current file" })
+        vim.keymap.set("n", "<leader>.", builtin.current_buffer_fuzzy_find, { desc = "Fuzzy find in current file" })
     end,
 }
 
--- lua/plug/whichkey.lua
+++ lua/plug/whichkey.lua
@@ -6,46 +6,37 @@ return {
         vim.o.timeoutlen = 550
         local wk = require("which-key")
         wk.setup({
+            preset = "classic",
+            ignore_missing = true,
+            show_help = false,
+            show_keys = false,
+            win = {
+                title = false,
+                border = "single",
+            },
             plugins = {
                 registers = false,
+                presets = {
+                    operators = false,    -- adds help for operators like d, y, ...
+                    motions = false,      -- adds help for motions
+                    text_objects = false, -- help for text objects triggered after entering an operator
+                    windows = false,      -- default bindings on <c-w>
+                    nav = false,          -- misc bindings to work with windows
+                },
             },
-            window = {
-                border = "single",        -- none, single, double, shadow
-                position = "bottom",      -- bottom, top
-                margin = { 1, 0, 0, 0 },  -- extra window margin [top, right, bottom, left]. When between 0 and 1, will be treated as a percentage of the screen size.
-                padding = { 1, 1, 1, 1 }, -- extra window padding [top, right, bottom, left]
-                winblend = 0,             -- value between 0-100 0 for fully opaque and 100 for fully transparent
-                zindex = 1000,            -- positive value to position WhichKey above other floating windows.
+            modes = {
+                i = false, -- Insert mode
             },
-            layout = {
-                height = { min = 4, max = 25 }, -- min and max height of the columns
-                width = { min = 20, max = 50 }, -- min and max width of the columns
-                spacing = 2,                    -- spacing between columns
-                align = "left",                 -- align columns left, center or right
-            },
-            triggers_nowait = {
-                "`",
-                "'",
-                '"',
-                "z=",
-                "za",
-                "zz",
-            },
-            ignore_missing = true,
-            show_help = false, -- show a help message in the command line for using WhichKey
-            show_keys = false, -- show the currently pressed key and its label as a message in the command line
         })
-        wk.register({
-            f = { name = "Files/Telescope", },
-            g = { name = "Lsp", },
-            W = { name = "Workspaces", },
-            w = { name = "Window", },
-            q = { name = "Quickfix list", },
-            j = { name = "Run test", },
-            d = { name = "Debug/Diagnostic", },
-           ["e"] = "which_key_ignore",
-        }, {
-            prefix = "<leader>"
+        wk.add({
+            { "<leader>f", group = "file" },
+            { "<leader>f", group = "Files/Telescope", },
+            { "<leader>g", group = "Lsp", },
+            { "<leader>W", group = "Workspaces", },
+            { "<leader>w", group = "Window", },
+            { "<leader>q", group = "Quickfix list", },
+            { "<leader>j", group = "Run test", },
+            { "<leader>d", group = "Debug/Diagnostic", },
         })
     end
 
