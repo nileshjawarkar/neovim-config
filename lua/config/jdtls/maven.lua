8ac953a1a4c5c4ad6dd5addc91a71d2d7ac71a29
nileshjawarkar <jawarkar.nilesh@gmail.com>
2024-07-14 16:46:11 +0530

    Updated JavaEE project creation impl

--- lua/config/jdtls/maven.lua
+++ lua/config/jdtls/maven.lua
@@ -104,6 +104,54 @@ m.get_pom = function(_, type, package, version, name)
     return str_pom
 end
 
+m.get_application_props = function(_)
+    return ""
+end
+
+m.get_persistence_xml = function(_)
+    local xml = [[
+<?xml version="1.0" encoding="UTF-8"?>
+<persistence version="2.2"
+	xmlns="http://xmlns.jcp.org/xml/ns/persistence"
+	xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+	xsi:schemaLocation="http://xmlns.jcp.org/xml/ns/persistence http://xmlns.jcp.org/xml/ns/persistence/persistence_2_2.xsd">
+	<persistence-unit name="prod" transaction-type="JTA">
+		<exclude-unlisted-classes>false</exclude-unlisted-classes>
+		<properties>
+			<property
+                name="jakarta.persistence.schema-generation.database.action"
+			    value="drop-and-create" />
+		</properties>		
+	</persistence-unit>
+</persistence>
+    ]]
+    return xml
+end
+
+m.get_beans_xml = function(_)
+    local xml = [[
+<?xml version="1.0" encoding="UTF-8"?>
+<beans xmlns="http://xmlns.jcp.org/xml/ns/javaee"
+    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+    xsi:schemaLocation="http://xmlns.jcp.org/xml/ns/javaee http://xmlns.jcp.org/xml/ns/javaee/beans_1_1.xsd"
+    bean-discovery-mode="all">
+</beans>
+    ]]
+    return xml
+end
+
+m.get_web_xml = function(_, app_name)
+    local xml = [[
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE web-app PUBLIC '-//Sun Microsystems, Inc.//DTD Web
+Application 2.3//EN' 'http://java.sun.com/dtd/web-app_2_3.dtd'>
+<web-app>
+	<display-name>]] .. app_name .. [[</display-name>
+</web-app>
+    ]]
+    return xml
+end
+
 m.create_prj = function(self, type, root_dir)
     local root_name = vim.fn.fnamemodify(root_dir, ':p:h:t')
     local is_jee = (type == "javaee")
@@ -133,6 +181,37 @@ m.create_prj = function(self, type, root_dir)
         if not st then
             print("Failed to create - " .. root_dir .. "/pom.xml")
             return
+        end
+
+        if is_jee then
+            st = sys.write_to(root_dir .. "/src/main/webapp/WEB-INF/web.xml", function(file)
+                file.write(self:get_web_xml(root_name))
+            end)
+            if not st then
+                print("Failed to create - " .. root_dir .. "/src/main/webapp/WEB-INF/web.xml")
+                return
+            end
+            st = sys.write_to(root_dir .. "/src/main/webapp/WEB-INF/beans.xml", function(file)
+                file.write(self:get_beans_xml())
+            end)
+            if not st then
+                print("Failed to create - " .. root_dir .. "/src/main/webapp/WEB-INF/beans.xml")
+                return
+            end
+            st = sys.write_to(root_dir .. "/src/main/resources/application.properties", function(file)
+                file.write(self:get_application_props())
+            end)
+            if not st then
+                print("Failed to create - " .. root_dir .. "/src/main/resources/application.properties")
+                return
+            end
+            st = sys.write_to(root_dir .. "/src/main/resources/META-INF/persistence.xml", function(file)
+                file.write(self:get_persistence_xml())
+            end)
+            if not st then
+                print("Failed to create - " .. root_dir .. "/src/main/resources/META-INF/persistence.xml")
+                return
+            end
         end
     end
 end
