e8d0149e7950adf6abe37e407ca2c41cf0208c35
nileshjawarkar <jawarkar.nilesh@gmail.com>
2024-08-09 21:53:36 +0530

    Modified xmlformat with indent size = 3

--- lua/plug/formatter.lua
+++ lua/plug/formatter.lua
@@ -10,63 +10,47 @@ return {
                 lua = {
                     require("formatter.filetypes.lua").stylua,
                 },
-
-                c = {
-                    {
-                        exe = "clang-format",
-                        args = { "--style=Microsoft" },
-                        stdin = true,
-                    },
-                },
-
-                cpp = {
-                    {
-                        exe = "clang-format",
-                        args = { "--style=Microsoft" },
-                        stdin = true,
-                    },
-                },
-
-                java = {
-                    {
-                        exe = "clang-format",
-                        args = { '--style="{BasedOnStyle: Google, IndentWidth: 4}"', "--assume-filename=.java" },
-                        stdin = true,
-                    },
-                },
-
+                c = { {
+                    exe = "clang-format",
+                    args = { "--style=Microsoft" },
+                    stdin = true,
+                } },
+                cpp = { {
+                    exe = "clang-format",
+                    args = { "--style=Microsoft" },
+                    stdin = true,
+                } },
+                java = { {
+                    exe = "clang-format",
+                    args = { '--style="{BasedOnStyle: Google, IndentWidth: 4}"', "--assume-filename=.java" },
+                    stdin = true,
+                } },
+                xml = { {
+                    exe = "xmlformat",
+                    args = { '--preserve "pre,literal" --blanks --selfclose --indent 3 --indent-char " " --disable-inlineformatting --disable-correction --eof-newline', '-', },
+                    stdin = true,
+                } },
                 json = {
                     require("formatter.filetypes.json").prettier,
                 },
-
                 yaml = {
                     require("formatter.filetypes.yaml").prettier,
                 },
-
                 javascript = {
                     require("formatter.filetypes.javascript").prettier,
                 },
-
                 typescript = {
                     require("formatter.filetypes.typescript").prettier,
                 },
-
                 html = {
                     require("formatter.filetypes.html").prettier,
                 },
-
                 css = {
                     require("formatter.filetypes.css").prettier,
                 },
-
-                xml = {
-                    require("formatter.filetypes.xml").xmlformat,
-                },
-
                 markdown = {
                     require("formatter.filetypes.markdown").prettier,
                 },
-
                 ["*"] = {
                     require("formatter.filetypes.any").remove_trailing_whitespace,
                 },
