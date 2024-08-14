b847c7f35749ae86a1df96236e3437ccccc7404f
nileshjawarkar <jawarkar.nilesh@gmail.com>
2024-08-14 21:49:03 +0530

    Reverted back to oil as a file manager

--- lua/config/filemanager.lua
+++ lua/config/filemanager.lua
@@ -1,4 +1,32 @@
 local m = {}
+
+m.setup_oil = function()
+    local oil = require("oil")
+    oil.setup({
+        view_options = {
+            show_hidden = false,
+            is_hidden_file = function(name, _)
+                if name == ".git" or name == ".." then
+                    return true
+                end
+                return false
+            end,
+        },
+    })
+    vim.keymap.set("n", "<leader>-", oil.open, { desc = "Locate current file" })
+    vim.keymap.set("n", "<leader>tl", oil.open, { desc = "Locate current file [<Leader>-]" })
+
+    vim.keymap.set("n", "<leader>_", function()
+        oil.open(vim.fn.getcwd())
+    end, { desc = "Open root folder" })
+    vim.keymap.set("n", "<leader>tr", function()
+        oil.open(vim.fn.getcwd())
+    end, { desc = "Open root folder [<Leader>_]" })
+
+    vim.keymap.set("n", "<leader>th", oil.toggle_hidden, { desc = "Show hidden files (toggle)" })
+end
+
+--[[
 m.setup = function()
     vim.g.loaded_netrw = 1
     vim.g.loaded_netrwPlugin = 1
@@ -33,10 +61,23 @@ m.setup = function()
     vim.keymap.set("n", "<leader>tk", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse" })
     vim.keymap.set("n", "<leader>tt", "<cmd>NvimTreeToggle<CR>", { desc = "Show/Toggle" })
     vim.keymap.set("n", "<leader>tr", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh" })
-end
+end ]]
+
+--[[
+m.setup_neotree = function()
+    require("neo-tree").setup({
+        close_if_last_window = false,
+        popup_border_style = "rounded",
+        enable_git_status = false,
+    })
+
+    vim.keymap.set("n", "<leader>tc", "<cmd>Neotree close<CR>", { desc = "Close view" })
+    vim.keymap.set("n", "<leader>tt", "<cmd>Neotree toggle<CR>", { desc = "Toggle view" })
+    vim.keymap.set("n", "<leader>tl", "<cmd>Neotree reveal<CR>", { desc = "Locate current file" })
+end ]]
 
 m.closeTreeView = function()
-    vim.cmd("NvimTreeClose")
+    -- vim.cmd("NvimTreeClose")
 end
 
 return m
--- lua/plug/filemanager.lua
+++ lua/plug/filemanager.lua
@@ -1,4 +1,13 @@
 return {
+    'stevearc/oil.nvim',
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {},
+    config = function()
+        require("config.filemanager").setup_oil()
+    end,
+}
+--[[
+return {
     "nvim-tree/nvim-tree.lua",
     event = "VeryLazy",
     dependencies = { "nvim-tree/nvim-web-devicons" },
@@ -6,46 +15,7 @@ return {
         require("config.filemanager").setup()
     end,
 }
---[[
-return {
-    'stevearc/oil.nvim',
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    opts = {},
-    config = function()
-        local oil = require("oil")
-        oil.setup({
-            float = {
-                padding = 5,
-                border = "rounded",
-                win_options = {
-                    winblend = 0,
-                },
-                preview_split = "auto",
-            },
-            view_options = {
-                show_hidden = false,
-                is_hidden_file = function(name, _)
-                    if name == ".nvim" or name == ".gitignore" then
-                        return false
-                    end
-                    return vim.startswith(name, ".")
-                end,
-            },
-        })
-        vim.keymap.set("n", "<leader>-", oil.open_float, { desc = "Locate current file" })
-        vim.keymap.set("n", "<leader>_", function()
-            oil.open_float(require("core.util.sys").find_root())
-        end, { desc = "Open root folder" })
-        vim.keymap.set("n", "<leader>tl", oil.open_float, { desc = "Locate current file" })
-        vim.keymap.set("n", "<leader>tr", function()
-            oil.open_float(require("core.util.sys").find_root())
-        end, { desc = "Open root folder" })
-        vim.keymap.set("n", "<leader>th", oil.toggle_hidden, { desc = "Show hidden files (toggle)" })
-    end,
-}
-]]
 
---[[
 return {
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v3.x",
@@ -55,15 +25,6 @@ return {
         "MunifTanjim/nui.nvim",
     },
     config = function()
-        require("neo-tree").setup({
-            close_if_last_window = false,
-            popup_border_style = "rounded",
-            enable_git_status = false,
-        })
-
-        vim.keymap.set("n", "<leader>tc", "<cmd>Neotree close<CR>", { desc = "Close view" })
-        vim.keymap.set("n", "<leader>tt", "<cmd>Neotree toggle<CR>", { desc = "Toggle view" })
-        vim.keymap.set("n", "<leader>tl", "<cmd>Neotree reveal<CR>", { desc = "Locate current file" })
     end,
 }
 ]]
