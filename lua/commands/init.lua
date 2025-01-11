f21d91b77ba66f724293ef83c24aaf56dec5ca5e
nileshjawarkar <jawarkar.nilesh@gmail.com>
2025-01-11 21:11:16 +0530

    Core Refactoring

--- lua/commands/init.lua
+++ lua/commands/init.lua
@@ -1,7 +1,5 @@
 require("commands.mvn")
 
-local sys = require("core.util.sys")
-
 -- Auto command to clear block cursor after exit. It help to prevent messup in terminals
 -- color theme. This is useful in alacrity teminal OR may also helful in other
 -- terminals.
@@ -13,19 +11,26 @@ vim.api.nvim_create_autocmd("ExitPre", {
 
 -- User commands
 vim.api.nvim_create_user_command("JavaVersion", function()
-    local v, err = require("core.rt.java").get_java_version()
-    if err == nil and v ~= nil then
-        print("Java version [" .. v.major .. "." .. v.minor .. "." .. v.patch .. "]")
+    local rt_java = require("core.rt.java")
+    if rt_java ~= nil then
+        local v, err = rt_java.get_java_version()
+        if err == nil and v ~= nil then
+            print("Java version [" .. v.major .. "." .. v.minor .. "." .. v.patch .. "]")
+        end
     end
 end, {})
 
 vim.api.nvim_create_user_command("PrjInitConfig", function()
-    local cur_dir = vim.fn.getcwd()
-    sys.create_project_config(cur_dir)
+    local sys = require("core.util.sys")
+    if sys ~= nil then
+        local cur_dir = vim.fn.getcwd()
+        sys.create_project_config(cur_dir)
+    end
 end, {})
 
 vim.api.nvim_create_user_command("PrjReloadConfig", function()
     local config = require("config.ws")
-    config.reload_config()
+    if config ~= nil then
+        config.reload_config()
+    end
 end, {})
-
