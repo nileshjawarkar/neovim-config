bdecb73693a1653bf812769679a95128c581c9d1
nileshjawarkar <jawarkar.nilesh@gmail.com>
2025-01-13 23:13:25 +0530

    Notify changes 1 draft

--- lua/plug/notify.lua
+++ lua/plug/notify.lua
@@ -1,43 +1,14 @@
 return {
     "rcarriga/nvim-notify",
     config = function()
-        local util = require("notify.stages.util")
-        local direction = util.DIRECTION.BOTTOM_UP
-        local notif_count = 0
-        local stage = {
-            function(state)
-                local next_height = state.message.height + notif_count
-                notif_count = notif_count + 1
-                local next_row = util.available_slot(state.open_windows, next_height, direction)
-                if not next_row then
-                    return nil
-                end
-                return {
-                    relative = "editor",
-                    anchor = "NE",
-                    width = state.message.width,
-                    height = state.message.height,
-                    col = vim.opt.columns:get(),
-                    row = next_row,
-                    border = "none",
-                }
-            end,
-            function()
-                notif_count = notif_count - 1
-                return {
-                    col = vim.opt.columns:get(),
-                    time = true,
-                }
-            end,
-        }
-
         local notify = require("notify")
         notify.setup({
-            max_height = 3,
+            max_height = 5,
             render = "wrapped-compact",
-            stages = stage,
+            stages = "fade_in_slide_out",
             timeout = 1700,
         })
         vim.notify = notify
+        vim.keymap.set("n", "<leader>N", notify.dismiss, { desc = "Clear notifications" })
     end
 }
