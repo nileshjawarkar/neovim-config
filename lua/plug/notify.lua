4b89e4726807b3dedea1c7ba6f63bfc76bfe1514
nileshjawarkar <jawarkar.nilesh@gmail.com>
2025-01-13 21:25:19 +0530

    Modified notify

--- lua/plug/notify.lua
+++ lua/plug/notify.lua
@@ -5,7 +5,9 @@ return {
         notify.setup({
             max_height = 3,
             top_down = false,
-            render = "wrapped-compact",
+            render = "compact",
+            stages = "fade",
+            timeout = 1200,
         })
         vim.notify = notify
     end
