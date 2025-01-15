bda72474b3a92e2cc51542c0f32b687dc399dd81
nileshjawarkar <jawarkar.nilesh@gmail.com>
2025-01-15 20:46:59 +0530

    modified notify

--- lua/plug/notify.lua
+++ lua/plug/notify.lua
@@ -4,10 +4,9 @@ return {
         local notify = require("notify")
         notify.setup({
             max_height = 5,
-            render = "wrapped-compact",
+            render = "compact",
             stages = "fade_in_slide_out",
             timeout = 1700,
-            top_down = false,
         })
         vim.notify = notify
         vim.keymap.set("n", "<leader>N", notify.dismiss, { desc = "Clear notifications" })
