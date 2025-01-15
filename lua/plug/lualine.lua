1751c989ee42ba5f91f546c4dd0f88c11824288e
nileshjawarkar <jawarkar.nilesh@gmail.com>
2025-01-15 22:12:59 +0530

    Modified lua line

--- lua/plug/lualine.lua
+++ lua/plug/lualine.lua
@@ -8,7 +8,7 @@ return {
         local function treeName()
             return [[FILES]]
         end
-        local nvimtree_ext = { sections = { lualine_a = { treeName }, lualine_z = { "location" }, }, filetypes = { 'NvimTree' } }
+        local nvimtree_ext = { sections = { lualine_a = { treeName }, lualine_b = { "branch" }, lualine_z = { "location" }, }, filetypes = { 'NvimTree' } }
 
         require("lualine").setup({
             options = {
@@ -16,22 +16,18 @@ return {
                 theme = "auto",
                 component_separators = { left = "", right = "" },
                 section_separators = { left = "", right = "" },
-                disabled_filetypes = {
-                    -- statusline = { "NvimTree", },
-                    -- winbar = { "NvimTree",},
-                },
                 ignore_focus = {},
                 always_divide_middle = true,
-                globalstatus = false,
+                globalstatus = true,
                 refresh = {
-                    statusline = 1000,
-                    tabline = 1000,
-                    winbar = 1000,
+                    statusline = 100,
+                    tabline = 200,
+                    winbar = 300,
                 },
             },
             sections = {
                 lualine_a = { "mode" },
-                lualine_b = { "branch", "diff", "diagnostics" },
+                lualine_b = { "diff", "diagnostics" },
                 lualine_c = { "filename" },
                 lualine_x = { "encoding", "filetype" },
                 lualine_y = {},
