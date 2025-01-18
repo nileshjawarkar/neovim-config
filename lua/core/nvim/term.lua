4c6db7525aba53d6555c601f687296afcda595d0
nileshjawarkar <jawarkar.nilesh@gmail.com>
2025-01-18 21:00:49 +0530

    Code Refactoring

--- lua/core/nvim/term.lua
+++ lua/core/nvim/term.lua
@@ -1,3 +1,14 @@
+local function split_horizontally(height)
+    -- Create a new horizontal split and open it
+    vim.api.nvim_command('new')
+    local win_id = vim.api.nvim_get_current_win()
+    -- Set height
+    if height ~= nil then
+        vim.api.nvim_win_set_height(win_id, height)
+    end
+    return win_id
+end
+
 -- Function to find the window with the maximum size
 local function find_max_size_window()
     local max_area = 0
@@ -15,36 +26,20 @@ local function find_max_size_window()
     return max_win_id
 end
 
--- Function to split the maximum size window and open terminal
-local function split_and_open_terminal(term_buf)
+local function split_max_size_window(min_size_percent)
     local max_win_id = find_max_size_window()
     if not max_win_id then
         print("No windows found.")
-        return
+        return nil
     end
-
     -- move cursor to max_win_id
     vim.api.nvim_set_current_win(max_win_id)
-
     -- manage 70, 30 split
     local total_lines = vim.api.nvim_win_get_height(0)
-    local smaller_window_lines = math.floor(total_lines * 0.30)
-    vim.cmd('split')
-    vim.cmd('resize ' .. smaller_window_lines)
-
-    -- move to smaller/lower window
-    vim.cmd('wincmd j')
-    -- we already has term buffer -> set it to current window
-    -- or else create new one
-    if term_buf ~= nil then
-        vim.api.nvim_set_current_buf(term_buf)
-    else
-        vim.cmd('terminal')
-    end
-
-    -- start insert mode
-    vim.cmd('startinsert')
+    local smaller_window_lines = math.floor(total_lines * min_size_percent)
+    return split_horizontally(smaller_window_lines)
 end
+
 local function is_buf_terminal(buf)
     local bufname = vim.api.nvim_buf_get_name(buf)
     if bufname:find("term://") then
@@ -53,38 +48,77 @@ local function is_buf_terminal(buf)
     return false
 end
 
+local function find_terminal_buf()
+    for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+        if is_buf_terminal(buf) then
+            return buf
+        end
+    end
+    return nil
+end
+
+-- Function to split the maximum size window and open terminal
+local function split_and_set_terminal(term_buf)
+    local win_id = split_max_size_window(0.30)
+    if win_id == nil then return end
+    vim.api.nvim_set_current_win(win_id)
+    -- we already has term buffer -> set it to current window
+    -- or else create new one
+    if term_buf ~= nil then
+        vim.api.nvim_set_current_buf(term_buf)
+    else
+        vim.cmd('terminal')
+        term_buf = vim.api.nvim_win_get_buf(0)
+    end
+    -- start insert mode
+    vim.cmd('startinsert')
+end
+
+
 local term_buf = nil
 local function open_or_hide_terminal()
-    -- Iterate over all buffers
-    if term_buf == nil or is_buf_terminal(term_buf) then
-        for _, buf in ipairs(vim.api.nvim_list_bufs()) do
-            if is_buf_terminal(buf) then
-                term_buf = buf
-                break
-            end
-        end
+    -- Step 1 - Find buffer of type terminal
+    -- Do it only if term_buf is nil and not of type term_buf
+    if term_buf == nil or not is_buf_terminal(term_buf) then
+        term_buf = find_terminal_buf()
     end
+    -- Step 2 - if no terminal buffer found..
+    -- split max size window and create new
+    if term_buf == nil then
+        split_and_set_terminal()
+        return
+    end
+    -- Step 3 - If terminal buffer exists,
+    -- check if it is hidden, if hidden split max sise window
+    -- and set it to new window.
 
-    -- If terminal buffer exists, use it
-    if term_buf then
-        local win_id = vim.fn.bufwinid(term_buf)
-        if win_id == -1 then
-            -- hidden windows
-            split_and_open_terminal(term_buf)
-        else
-            vim.api.nvim_win_hide(win_id)
-        end
-    else
-        split_and_open_terminal()
+    -- Here win_id == -1, indicate hidden window
+    local win_id = vim.fn.bufwinid(term_buf)
+    if win_id == -1 then
+        split_and_set_terminal(term_buf)
+        return
     end
+    -- Step 4 - If we are here, it means it  is active window
+    -- and hide it for toggle
+    vim.api.nvim_win_hide(win_id)
 end
 
 local function toggleTerm()
+    -- exit terminal mode
     if vim.bo.buftype == 'terminal' then
         vim.api.nvim_feedkeys([[<C-\><C-n>]], 'n', true)
     end
+    -- open or hide terminal
     open_or_hide_terminal()
 end
 
 vim.keymap.set({ "n" }, "<Leader>T", toggleTerm, { noremap = true, silent = true, desc = "Terminal (C-t)" })
 vim.keymap.set({ "n", "t" }, "<C-t>", toggleTerm, { noremap = true, silent = true })
+
+return {
+    split_horizontally = split_horizontally,
+    find_max_size_window = find_max_size_window,
+    split_max_size_window = split_max_size_window,
+    is_buf_terminal = is_buf_terminal,
+    find_terminal_buf = find_terminal_buf,
+}
