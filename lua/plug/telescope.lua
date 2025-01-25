04e41eb427a3191dd8676193eb569f1853a5d186
nileshjawarkar <jawarkar.nilesh@gmail.com>
2025-01-25 14:36:50 +0530

    telescope modified

--- lua/plug/telescope.lua
+++ lua/plug/telescope.lua
@@ -68,17 +68,22 @@ return {
 
         local function prepare_handler(fun, args)
             return function()
-                if not execute_checks_and_preqs() then
-                    return
-                end
+                if not execute_checks_and_preqs() then return end
                 if args ~= nil then
                     fun(args())
-                else
-                    fun()
+                    return
                 end
+                fun()
             end
         end
 
+        local function buf_search_op()
+            return {
+                previewer = false,
+                search_dirs = { vim.fn.expand('%:p'), },
+                prompt_title = 'Search in current file',
+            }
+        end
         local function key_ops(desc)
             return { noremap = true, silent = true, desc = desc }
         end
@@ -92,12 +97,15 @@ return {
         local show_recentfile = prepare_handler(builtin.oldfiles)
         local show_help = prepare_handler(builtin.help_tags)
         local fuzzy_find_tuc_in_ws = prepare_handler(builtin.grep_string)
-        local fuzzy_find_tuc_in_cur_buf = prepare_handler(builtin.grep_string, function()
-            return { search_dirs = { vim.fn.expand('%:p'), } }
-        end)
-
+        local fuzzy_find_tuc_in_cur_buf = prepare_handler(builtin.grep_string, buf_search_op)
+        local find_in_cur_buf = prepare_handler(builtin.live_grep, buf_search_op)
         local live_grep = prepare_handler(require('telescope').extensions.live_grep_args.live_grep_args)
-
+        local find_in_open_bufs = prepare_handler(function()
+            builtin.live_grep {
+                grep_open_files = true,
+                prompt_title = 'Search in open files',
+            }
+        end)
         vim.keymap.set("n", "<leader>ff", find_files, key_ops("Fuzzy find files [<Leader><Leader>]"))
         vim.keymap.set("n", "<leader><leader>", find_files, key_ops("Fuzzy find files"))
         vim.keymap.set("n", "<leader>fR", show_recentfile, key_ops("Find recent open files"))
@@ -107,32 +115,16 @@ return {
         vim.keymap.set("n", "<leader>fw", fuzzy_find_tuc_in_cur_buf, key_ops("Find text under cursor (in buffer)"))
         vim.keymap.set("n", "<leader>fb", show_buffers, key_ops("List open files [<Leader>,]"))
         vim.keymap.set("n", "<leader>,", show_buffers, key_ops("List open files"))
-        vim.keymap.set('n', '<leader>fd', find_diagnostics, { desc = 'List diagnostics' })
-        vim.keymap.set('n', '<leader>fr', resume, { desc = 'Resume search' })
-
-        local find_in_cur_buf = prepare_handler(function()
-            builtin.live_grep {
-                search_dirs = { vim.fn.expand('%:p'), },
-                previewer = false,
-                prompt_title = 'Search in current file',
-            }
-        end)
-        vim.keymap.set('n', '<leader>f.', find_in_cur_buf, { desc = 'Find in buffer' })
-        vim.keymap.set('n', '<leader>.', find_in_cur_buf, { desc = 'Find in buffer' })
-
-        local find_in_open_bufs = prepare_handler(function()
-            builtin.live_grep {
-                grep_open_files = true,
-                previewer = false,
-                prompt_title = 'Search in open files',
-            }
-        end)
-        vim.keymap.set('n', '<leader>f/', find_in_open_bufs, { desc = 'Find in Open Files' })
-        vim.keymap.set('n', '<leader>/', find_in_open_bufs, { desc = 'Find in Open Files' })
+        vim.keymap.set('n', '<leader>fd', find_diagnostics, key_ops('List diagnostics'))
+        vim.keymap.set('n', '<leader>fr', resume, key_ops('Resume search'))
+        vim.keymap.set('n', '<leader>f.', find_in_cur_buf, key_ops('Find in buffer'))
+        vim.keymap.set('n', '<leader>.', find_in_cur_buf, key_ops('Find in buffer'))
+        vim.keymap.set('n', '<leader>f/', find_in_open_bufs, key_ops('Find in open files'))
+        vim.keymap.set('n', '<leader>/', find_in_open_bufs, key_ops('Find in open files'))
 
         -- Shortcut for searching your Neovim configuration files
         vim.keymap.set('n', '<leader>fn', function()
             builtin.find_files { cwd = vim.fn.stdpath 'config' }
-        end, { desc = 'Search neovim files' })
+        end, key_ops('Search neovim files'))
     end,
 }
