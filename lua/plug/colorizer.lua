f359ae3621f3dad14beff63f373ccfa3c3db327b
nileshjawarkar <jawarkar.nilesh@gmail.com>
2025-02-01 20:42:05 +0530

    Added some more file-types

--- lua/plug/colorizer.lua
+++ lua/plug/colorizer.lua
@@ -2,23 +2,16 @@ return {
     "norcalli/nvim-colorizer.lua",
     config = function()
         local opts = {
-            RGB      = true, -- #RGB hex codes
-            RRGGBB   = true, -- #RRGGBB hex codes
-            names    = true, -- "Name" codes like Blue
-            RRGGBBAA = true, -- #RRGGBBAA hex codes
-            rgb_fn   = true, -- CSS rgb() and rgba() functions
-            hsl_fn   = true, -- CSS hsl() and hsla() functions
-            css      = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
-            css_fn   = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+            RGB      = true, RRGGBB   = true,
+            names    = true, RRGGBBAA = true,
+            rgb_fn   = true, hsl_fn   = true,
+            css      = true, css_fn   = true,
         }
-
         require 'colorizer'.setup {
-            scss = opts,
-            css = opts,
+            scss = opts, css = opts,
             javascript = opts,
-            html = {
-                mode = 'foreground',
-            }
+            yaml = opts, svg = opts,
+            html = { mode = 'foreground', }
         }
     end
 }
