6e95912a5a2c9138a0f3b7d08ea844fb49369af6
nileshjawarkar <jawarkar.nilesh@gmail.com>
2025-02-04 19:27:54 +0530

    Refactor

--- lua/plug/treesitter.lua
+++ lua/plug/treesitter.lua
@@ -1,7 +1,7 @@
 return {
     'nvim-treesitter/nvim-treesitter',
     config = function()
-        require 'nvim-treesitter.configs'.setup({
+        require("nvim-treesitter.configs").setup({
             ensure_installed = require("config.req_tools").treesitter_req,
             sync_install = false,
             auto_install = false,
