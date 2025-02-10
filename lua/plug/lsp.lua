2f4b57188ff7ffe0cd0675b92bfcc952b628d2fa
nileshjawarkar <jawarkar.nilesh@gmail.com>
2025-02-10 19:23:57 +0530

    Removed fidget plugin

--- lua/plug/lsp.lua
+++ lua/plug/lsp.lua
@@ -4,9 +4,6 @@ return {
         "williamboman/mason.nvim",
         "williamboman/mason-lspconfig.nvim",
         "WhoIsSethDaniel/mason-tool-installer.nvim",
-
-        -- Useful status updates for LSP.
-        { 'j-hui/fidget.nvim', opts = {} },
         -- completion
         "hrsh7th/cmp-nvim-lsp", -- LSP source for nvim-cmp
     },
