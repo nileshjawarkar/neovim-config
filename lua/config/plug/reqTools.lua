c27a8a5904f31cd6b7fe14c59fd9224c9a6e3ef4
nileshjawarkar <jawarkar.nilesh@gmail.com>
2025-04-11 21:24:47 +0530

    Random refactoring

---
+++ lua/config/plug/reqTools.lua
@@ -0,0 +1,22 @@
+return {
+    treesitter_req = {
+        "lua", "vim", "vimdoc", -- Basic/Tools
+        "markdown", "markdown_inline", -- Notes
+        "json", "yaml", "xml", "toml",
+        "make", "c", "cpp", "java",     -- languages & buildtool
+        "css", "html", "javascript", -- Web
+        -- "c_sharp", -- languages optional
+    },
+
+    mason_req = {
+        "lua_ls", "jsonls", "yamlls",
+        "bashls", "dockerls",
+        "jdtls", "java-debug-adapter", "java-test", -- Java
+        "clangd", "codelldb", "cpptools", "cpplint",  -- C, CPP
+        "clang-format", "prettier", "xmlformatter",  -- Formatting
+        "codespell", -- check spells in code
+        -- "quick_lint_js", "cssls", "ts_ls", "emmet-language-server", -- Web
+        -- "pyright", -- Python
+    },
+
+}
