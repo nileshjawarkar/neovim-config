613a0a8c33014a57267bba6aa02afc0f0ba4aa64
nileshjawarkar <jawarkar.nilesh@gmail.com>
2025-04-11 21:04:37 +0530

    Added bigfile plugin

--- lua/plug/other.lua
+++ lua/plug/other.lua
@@ -3,9 +3,15 @@ return {
         'folke/lazydev.nvim',
         ft = 'lua',
         opts = {
-            library = {
-                { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
-            },
+            library = { { path = '${3rd}/luv/library', words = { 'vim%.uv' } }, },
         },
     },
+    {
+        'LunarVim/bigfile.nvim',
+        event = 'BufReadPre',
+        opts = { filesize = 2, },
+        config = function(_, opts)
+            require('bigfile').setup(opts)
+        end
+    }
 }
