7769e94a01831c5b4bfe2dd402b392855e4757c3
nileshjawarkar <jawarkar.nilesh@gmail.com>
2025-05-07 09:53:36 +0530

    Some updates

--- lua/plug/blink_cmp.lua
+++ lua/plug/blink_cmp.lua
@@ -44,8 +44,9 @@ return {
 
         completion = {
             -- By default, you may press `<c-space>` to show the documentation.
-            -- Optionally, set `auto_show = true` to show the documentation after a delay.
-            documentation = { auto_show = false, auto_show_delay_ms = 500 },
+            documentation = { auto_show = false, },
+            ghost_text = { enabled = true },
+            list = { selection = { preselect = true, auto_insert = true } },
             menu = {
                 border = "rounded",
             },
