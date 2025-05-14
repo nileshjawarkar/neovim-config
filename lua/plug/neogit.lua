e564009373608ee74179ede117011ece70786379
nileshjawarkar <jawarkar.nilesh@gmail.com>
2025-05-14 09:45:35 +0530

    Delayed neogit loading

--- lua/plug/neogit.lua
+++ lua/plug/neogit.lua
@@ -1,6 +1,16 @@
 return {
     "NeogitOrg/neogit",
-    event = "VeryLazy",
+    keys = {
+        {
+            '<leader>G',
+            function()
+                require("config.handlers").closeThemForMe("git")
+                require('neogit').open({ kind = "replace" })
+            end,
+            mode = '',
+            desc = 'Open git view',
+        },
+    },
     dependencies = {
         "nvim-lua/plenary.nvim",
         "sindrets/diffview.nvim",
@@ -16,9 +26,11 @@ return {
                 section = { "+", "" },
             },
         })
+        --[[
         vim.keymap.set("n", "<leader>G", function()
             require("config.handlers").closeThemForMe("git")
             neogit.open({ kind = "replace" })
         end, { desc = "Open git view" })
+        ]]
     end,
 }
